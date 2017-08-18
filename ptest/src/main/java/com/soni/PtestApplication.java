890305cab83a2f708be9160d6ac113af939a7f24
vishal.soni <creditfinch@gmail.com>
2017-08-18 11:59:10 +0530

    Commit 1

--- ptest/src/main/java/com/soni/PtestApplication.java
+++ ptest/src/main/java/com/soni/PtestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PtestApplication {
-
+	// Change 1
 	public static void main(String[] args) {
 		SpringApplication.run(PtestApplication.class, args);
 	}
